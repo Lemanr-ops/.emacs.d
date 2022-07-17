9acbf35d19044e13ffe05d2ce501eb92b2080ad2
Lemanr-ops <71096974+Lemanr-ops@users.noreply.github.com>
2022-07-17 07:43:50 -0400

    Add files via upload

--- startup/aaaMain-Config.el
+++ startup/aaaMain-Config.el
@@ -1,4 +1,3 @@
-
 (setq inhibit-startup-message t)
 
 (tool-bar-mode 1)          ; Disable the toolbar
@@ -10,9 +9,6 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Fira Code Retina")
-
-(load-theme 'waher)
 
 ;; Initialize package sources
 (require 'package)
@@ -32,7 +28,9 @@
 (setq use-package-always-ensure t)
 
 
-
+(unless (package-installed-p 'waher-theme)
+  (package-install 'waher-theme))
+(load-theme 'waher)
 ;self explainatory
 (column-number-mode)
 ;row/column numbers in mode line
@@ -65,97 +63,102 @@
   :config
   (setq which-key-idle-delay 2))
 
-; adds additional info to M-x commands
-(use-package ivy-rich
-  :init (ivy-rich-mode 1))
 
 ; gives extra info to M-x functions
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
 	 ("C-x C-f" . counsel-find-file)
 	 :map minibuffer-local-map
 	 ("C-r" . 'counsel-minibuffer-history)))
-;avoid asynch
+; adds additional info to M-x commands
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+;avoid asynch buffer creation
 (add-to-list 'display-buffer-alist
   (cons "\\*Async Shell Command\\*.*" (cons #'display-buffer-no-window nil)))
 ;===From Emacs Desktop environment #2 improving
 ;setup brightness and volumn keys
-(use-package desktop-environment
-  :after exwm
-  :config (desktop-environment-mode)
-  :custom
-  (desktop-environment-brightness-small-increment "2%+")
-  (desktop-environment-brightness-small-decrement "2%-")
-  (desktop-environment-brightness-normal-increment "5%+")
-  (desktop-environment-brightness-normal-dncrement "5%-"))
+;(use-package desktop-environment
+;  :after exwm
+;  :config (desktop-environment-mode)
+;  :custom
+;  (desktop-environment-brightness-small-increment "2%+")
+;  (desktop-environment-brightness-small-decrement "2%-")
+;  (desktop-environment-brightness-normal-increment "5%+")
+;  (desktop-environment-brightness-normal-dncrement "5%-"))
 
 
 ;EXWM setup
 ;Needed for system tray, able to run apps in background
-(defun efs/run-in-background (command)
-  (let ((command-parts (split-string command "[ ]+")))
-    (apply #'call-process '(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
-(defun efs/exwm-init-hook ()
-  (exwm-workspace-switch-create 1)
+;(defun efs/run-in-background (command)
+;  (let ((command-parts (split-string command "[ ]+")))
+;    (apply #'call-process '(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
+;(defun efs/exwm-init-hook ()
+;  (exwm-workspace-switch-create 1)
 
-(efs/run-in-background "nm-applet")) ;Experimenting with
+;(efs/run-in-background "nm-applet")) ;Experimenting with
 
-(defun efs/exwm-update-class ()
-  (exwm-workspace-rename-buffer exwm-class-name))
+;(defun efs/exwm-update-class ()
+;  (exwm-workspace-rename-buffer exwm-class-name))
 
-(use-package exwm
-  :config
+;(use-package exwm
+;  :config
   ;; Set the default number of workspaces
-  (setq exwm-workspace-number 5)
+;  (setq exwm-workspace-number 5)
 
   ;; When window "class" updates, use it to set the buffer name
-  (add-hook 'exwm-update-class-hook #'efs/exwm-update-class)
+;  (add-hook 'exwm-update-class-hook #'efs/exwm-update-class)
 
   ;; These keys should always pass through to Emacs
-  (setq exwm-input-prefix-keys
-    '(?\C-x
-      ?\C-u
-      ?\C-h
-      ?\M-x
-      ?\M-`
-      ?\M-&
-      ?\M-:
-      ?\C-\M-j  ;; Buffer list
-      ?\C-\ ))  ;; Ctrl+Space
+;  (setq exwm-input-prefix-keys
+;    '(?\C-x
+;      ?\C-u
+;      ?\C-h
+;      ?\M-x
+;      ?\M-`
+;      ?\M-&
+;      ?\M-:
+;      ?\C-\M-j  ;; Buffer list
+;      ?\C-\ ))  ;; Ctrl+Space
 
   ;; Ctrl+Q will enable the next key to be sent directly
-  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+;  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
 
   ;; Set up global key bindings.  These always work, no matter the input state!
   ;; Keep in mind that changing this list after EXWM initializes has no effect.
-  (setq exwm-input-global-keys
-        `(
+;  (setq exwm-input-global-keys
+;        `(
           ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
-          ([?\s-r] . exwm-reset)
+;          ([?\s-r] . exwm-reset)
 
           ;; Move between windows
-          ([s-left] . windmove-left)
-          ([s-right] . windmove-right)
-          ([s-up] . windmove-up)
-          ([s-down] . windmove-down)
+;          ([s-left] . windmove-left)
+;          ([s-right] . windmove-right)
+;          ([s-up] . windmove-up)
+;          ([s-down] . windmove-down)
 
           ;; Launch applications via shell command
-          ([?\s-&] . (lambda (command)
-                       (interactive (list (read-shell-command "$ ")))
-                       (start-process-shell-command command nil command)))
+;          ([?\s-&] . (lambda (command)
+;                       (interactive (list (read-shell-command "$ ")))
+;                       (start-process-shell-command command nil command)))
 
           ;; Switch workspace
-          ([?\s-w] . exwm-workspace-switch)
+;          ([?\s-w] . exwm-workspace-switch)
 
-          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
-          ,@(mapcar (lambda (i)
-                      `(,(kbd (format "s-%d" i)) .
-                        (lambda ()
-                          (interactive)
-                          (exwm-workspace-switch-create ,i))))
-                    (number-sequence 0 9))))
-
-  (exwm-enable))
-
+;          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+;          ,@(mapcar (lambda (i)
+;                      `(,(kbd (format "s-%d" i)) .
+;                        (lambda ()
+;                          (interactive)
+;                          (exwm-workspace-switch-create ,i))))
+;                    (number-sequence 0 9))))
+;  (exwm-enable))
 (display-time-mode)
+;(exwm-init)
+
+;setup emacs as commonlisp IDE with SLIME
+;remember to do this in terminal sbcl --eval '(ql:quickload :quicklisp-slime-helper)' --quit
+(unless (package-installed-p 'slime)
+  (package-install 'slime))
+(setq inferior-lisp-program "sbcl")
